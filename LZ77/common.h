e1543779fef65b93a08fecabfb9b72b6567de216
akh5 <akmapengfei@163.com>
2020-03-02 20:23:05 +0800

    lz77/字节配置

---
+++ LZ77/common.h
@@ -0,0 +1,11 @@
+#pragma once
+#pragma warning(disable:4996)
+
+typedef unsigned char UCH;
+typedef unsigned short USH;
+typedef unsigned long long ULL;
+
+const USH MIN_MAT = 3;
+const USH MAX_MAT = 258;
+const USH WSIZE = 32 * 1024;
+
